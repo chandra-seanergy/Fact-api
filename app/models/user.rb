aa094217e842db8d87967fc57c33fbbe3abd7db8
DhruvRSeanergy <dhruv.r@seanergydigital.com>
2019-10-21 11:12:19 +0530

    Password Reset Using Token
    Reset Password Using Token generated during forget password API Hit

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,20 @@ class User < ApplicationRecord
   validates_presence_of :name, :username , on: :create
   validates :username, uniqueness: true
 
-  def password_token_valid?
-    (self.reset_password_sent_at + 4.hours) > Time.now.utc
-  end
+def generate_password_token!
+ self.reset_password_token = generate_token
+ self.reset_password_sent_at = Time.now.utc
+ save!
+end
+
+def password_token_valid?
+ (self.reset_password_sent_at + 4.hours) > Time.now.utc
+end
+
+def reset_password!(password)
+ self.reset_password_token = nil
+ self.password = password
+ save!
+end
 
 end
