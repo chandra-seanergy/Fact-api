73d3763923d95e4ee0c362bf3b151358612716b8
DhruvRSeanergy <dhruv.r@seanergydigital.com>
2019-10-30 01:38:08 +0530

    Comments in Code

--- app/controllers/api/v1/group_listing_controller.rb
+++ app/controllers/api/v1/group_listing_controller.rb
@@ -1,30 +1,35 @@
 class Api::V1::GroupListingController < ApplicationController
   before_action :authenticate_request!
 
+  # Fetch list of all groups where the visibility is public
   def public_groups
     groups = Group.public_groups
     render json: {status: 200, message: "Group list feched successfully.", groups: groups.map{|x|
        x.attributes.merge(avatar: x.avatar.url, member_count: x.group_members.length)}}
   end
 
+  # Fetch list of all groups where the visibility is internal
   def internal_groups
     groups = Group.internal_groups
     render json: {status: 200, message: "Group list feched successfully.", groups: groups.map{|x|
        x.attributes.merge(avatar: x.avatar.url, member_count: x.group_members.length)}}
   end
 
+  # API to accept parameters in object-attribute format and return list of Users
   def user_list
     users = User.search_users(params[:user][:credential])
     render json: {status: 200, message: "member list fecthed successfully.", members: users.map{|x|
       x.attributes.merge(avatar: x.avatar.url)}}
   end
 
+  # API to accept parameters without object-attribute format and return list of Users
   def user_list_simple
     users = User.search_users(params[:credential])
     render json: {status: 200, message: "member list fecthed successfully.", members: users.map{|x|
       x.attributes.merge(avatar: x.avatar.url)}}
   end
 
+  # API To fetch all groups where the user is owner or is a member of
   def your_groups
     all_groups = @current_user.your_groups
     render json: {status: 200, message: "Group list feched successfully.", groups: all_groups.map{|x|
--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -2,10 +2,11 @@ class Api::V1::GroupsController < ApplicationController
   before_action :authenticate_request!
   before_action :find_group, only: [:show, :destroy, :update]
 
+  # API to create groups
   def create
     group, group_avatar = Group.new(group_params), params[:group][:avatar]
     group.owner_id = @current_user.id
-    group.avatar = group_avatar if !group_avatar.nil? and File.exist?(group_avatar)
+    group.avatar = group_avatar if !group_avatar.nil? and File.exist?(group_avatar) #Add Avatar attribute only if valid file exists in params
     if group.save
       render json: {status: 200, message: "Group created successfully.", group: group}
     else
@@ -13,10 +14,12 @@ class Api::V1::GroupsController < ApplicationController
     end
   end
 
+  # Display group details
   def show
     render json: {status: 200, group: @group}
   end
 
+  # Delete group from database
   def destroy
     if @group.destroy
       render json: {status: 200, message: "Group deleted successfully."}
@@ -25,6 +28,7 @@ class Api::V1::GroupsController < ApplicationController
     end
   end
 
+  # Update Group Attributes
   def update
     group_avatar = params[:group][:avatar]
     @group.avatar = group_avatar if !group_avatar.nil? and File.exist?(group_avatar)
@@ -36,11 +40,12 @@ class Api::V1::GroupsController < ApplicationController
   end
 
   private
-
+  # Allow only Strong parameters to be passed to model
   def group_params
     params.require(:group).permit(:name, :description, :visibility)
   end
 
+  # Find groups on the basis of id recieved in parameter
   def find_group
     @group = Group.find_by(id: params[:id])
     render json: {status: 500, message: "No record found."} unless @group
